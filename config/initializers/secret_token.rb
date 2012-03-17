78640a3ca3e8b55e9b133f665e0ebd4933e757e0
Greg Gross <greg@gtgross.com>
2012-03-17 18:57:37 +1100

    added simple test rails app with unit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsRailsTest::Application.config.secret_token = '44ea4252438ea0f14ca050693451498acedf6fd49ca6a45885c92d90023f541e1fe4592298dc12b9489d299ca40376a03661e3e66a6e6cf3cfbf8ff8d44cbfe9'
